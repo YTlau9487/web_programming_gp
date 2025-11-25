7299396639a4220174019a13585c326d54eb2a86
YTlau <liul7784@gmail.com>
2025-11-25 16:41:41 +0800

    Add simple db

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,12 +1,14 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { nanoid } from 'nanoid';
 
 export const user = sqliteTable('user', {
-	id: text('id').primaryKey(),
+	id: text('id').primaryKey().$defaultFn(crypto.randomUUID),
+	username: text().notNull(),
 	age: integer('age')
 });
 
 export const session = sqliteTable('session', {
-	id: text('id').primaryKey(),
+	id: text('id').primaryKey().$defaultFn(nanoid),
 	userId: text('user_id').notNull().references(() => user.id),
 	expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
 });
