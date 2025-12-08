2b9a848c5a238ca28ae2e56e169dc3da48845022
YTlau <liul7784@gmail.com>
2025-12-08 17:00:27 +0800

    change the styling of header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { User } from '$lib/server/db/schema';
+  import { ShoppingBag } from 'lucide-svelte';
   export let user: User | null = null;
   $: username = user?.username ?? 'Guest';
 </script>
@@ -7,7 +8,10 @@
 <nav
   class="bg-black text-white h-15 flex flex-row flex-nowrap items-center px-36 py-1.5 text-base sticky top-0"
 >
-  <div class="flex-3">MarketPlace</div>
+  <div class="flex flex-3 items-center gap-2">
+    <ShoppingBag class="inline-block"/>
+    <span class="align-center">MarketPlace</span>
+  </div>
   <div class="flex flex-2 shrink-0 gap-9 justify-end">
     <span>Welcome, {username}</span>
 
